f412cfb470042730a0121cbb40a54b162e923060
Christian Nagel <christian@christiannagel.com>
2022-02-12 19:53:13 +0100

    file scoped namespace

--- csharp10andnext/BooksAPIv6/BooksAPI/Models/BooksContext.cs
+++ csharp10andnext/BooksAPIv6/BooksAPI/Models/BooksContext.cs
@@ -2,29 +2,28 @@
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
-namespace BooksAPI.Models
+namespace BooksAPI.Models;
+
+public class BooksContext : DbContext
 {
-    public class BooksContext : DbContext
+    public BooksContext(DbContextOptions<BooksContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public BooksContext(DbContextOptions<BooksContext> options)
-            : base(options) { }
+        modelBuilder.Entity<Book>().Property(b => b.Title).HasMaxLength(50);
+        modelBuilder.Entity<Book>().Property(b => b.Publisher).HasMaxLength(20);
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Book>().Property(b => b.Title).HasMaxLength(50);
-            modelBuilder.Entity<Book>().Property(b => b.Publisher).HasMaxLength(20);
-
-            Book[] sampleBooks = Enumerable.Range(1, 100)
-                .Select(i => new Book(i, $"title {i}", "sample pub"))
-                .ToArray();
-            modelBuilder.Entity<Book>().HasData(sampleBooks);
-        }
-
-        public DbSet<Book> Books => Set<Book>();
+        Book[] sampleBooks = Enumerable.Range(1, 100)
+            .Select(i => new Book(i, $"title {i}", "sample pub"))
+            .ToArray();
+        modelBuilder.Entity<Book>().HasData(sampleBooks);
     }
 
-    public record Book(
-        int BookId, 
-        [StringLength(50)] string Title,
-        [StringLength(20)] string Publisher);
+    public DbSet<Book> Books => Set<Book>();
 }
+
+public record Book(
+    int BookId, 
+    [StringLength(50)] string Title,
+    [StringLength(20)] string Publisher);
