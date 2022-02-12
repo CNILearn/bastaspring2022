55f44a95df8226fb2d1c1b8521f84b56ab9d8a97
Christian Nagel <christian@christiannagel.com>
2022-02-12 19:25:46 +0100

    cleanup namespaces

--- csharp10andnext/BooksAPI/BooksAPI/Controllers/BooksController.cs
+++ csharp10andnext/BooksAPI/BooksAPI/Controllers/BooksController.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using BooksAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using BooksAPI.Models;
 
 namespace BooksAPI.Controllers
 {
