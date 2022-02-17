a9dfb4a661478b8d9c554527fc65e22d0bdb89e3
Christian Nagel <christian@christiannagel.com>
2022-02-17 06:54:41 +0100

    lambda

---
+++ csharp10andnext/NaturalDelegateTypeForLambda/NaturalDelegateTypeForLambda/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// C# 9
+Func<int, bool> isEven = (int n) => n % 2 == 0;
+
+// C# 10 - infer delegate type
+// var isEven = (int n) => n % 2 == 0;
+
+if (isEven(34))
+{
+    Console.WriteLine($"34 is even");
+}
+
+
+// C# 10
+// Delegate d1 = bool (int n) => n % 2 == 0;
+
+// C# 9
+
+Delegate d1 = (Func<int, bool>)((int n) => n % 2 == 0);
